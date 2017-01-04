a4cad5663c7b435e8f5c507d3f62d9021f7569cb
hanseung <misojiwon3@naver.com>
2017-01-04 09:42:16 +0900

    web socket test2

---
+++ app/domain/ws/InEvent.scala
@@ -0,0 +1,10 @@
+package domain.ws
+
+/**
+  * Created by hanseung on 2017-01-03.
+  */
+case class InEvent(name: String, email: String)
+
+object InEvent {
+
+}
---
+++ app/domain/ws/OutEvent.scala
@@ -0,0 +1,10 @@
+package domain.ws
+
+/**
+  * Created by hanseung on 2017-01-03.
+  */
+case class OutEvent(message: String)
+
+object OutEvent {
+
+}
